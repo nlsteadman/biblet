b4f28cb6a80c8cb75b7a9d63083ca1094f1f4efe
Nicole Steadman <nsteadman@gmail.com>
2022-07-05 20:53:53 -0700

    trying to get cors.rb to work

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,15 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000, http://biblet.herokuapp.com'
+    origins 'http://localhost:4000'
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'https://biblet.herokuapp.com'
 
     resource "*",
       headers: :any,
